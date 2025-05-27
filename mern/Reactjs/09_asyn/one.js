dbae7d5eab699abdbba15012e912f4e479ef10bc
Ashantfet <kumarashant7767@gmail.com>
2025-05-27 18:40:02 +0530

    events async

---
+++ mern/Reactjs/09_asyn/one.js
@@ -0,0 +1,20 @@
+//async code
+
+//javascript is single threaded
+//synchronous code
+//slow as compared to asynchronous code
+//execution context
+//execute one line of code at a time
+//console log 1 followed by console log 2
+//call stack  memory heap
+
+//blocking code vs non-blocking code
+//blocking code: code that blocks the execution of other code  : read file synchronously  it will wait for the file to be read before executing the next line of code
+//non-blocking code: code that does not block the execution of other code : read file asynchronously it will not wait for the file to be read before executing the next line of code
+
+//database : file
+//JS engine   ::-> web api -> callback queue -> event loop -> call stack
+
+//set timeout :0 it will be little slower than the synchronous code
+
+
