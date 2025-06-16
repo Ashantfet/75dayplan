fd70f56005f0f3bfee7d15b6364db3ce9e836fe0
Ashantfet <kumarashant7767@gmail.com>
2025-06-16 19:18:43 +0530

    commit

--- adsa/Day5/max1count.py
+++ adsa/Day5/max1count.py
@@ -1,14 +1,24 @@
 class Solution:
+    # def findMaxConsecutiveOnes(self, nums: list[int]) -> int:
+    #     n = len(nums)
+    #     startpoint = 0
+    #     ans = []
+    #     for i in range(n):
+    #         if nums[i] == 0:
+    #             ans.append(i - startpoint)
+    #             startpoint = i + 1
+    #     ans.append(n - startpoint)
+    #     return max(ans)
     def findMaxConsecutiveOnes(self, nums: list[int]) -> int:
-        n = len(nums)
-        startpoint = 0
-        ans = []
-        for i in range(n):
-            if nums[i] == 0:
-                ans.append(i - startpoint)
-                startpoint = i + 1
-        ans.append(n - startpoint)
-        return max(ans)
+        count = 0
+        max_count =0
+        for i in range(0,len(nums)):
+            if nums[i] == 1:
+                count+=1
+            else:
+                max_count =max(max_count,count)
+                count = 0
+        return max(max_count,count)
 
 a = list(map(int, input().split()))
 sol = Solution()
--- adsa/Day5/twosum.py
+++ adsa/Day5/twosum.py
@@ -1,11 +1,11 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         hashmap = {}
-        for i, num in enumerate(nums):
-            diff = target - num
+        for i in range(0,len(nums)):
+            diff = target - nums[i]
             if diff in hashmap:
                 return [hashmap[diff], i]
-            hashmap[num] = i
+            hashmap[nums[i]] = i
 
 arr = list(map(int, input().split()))
 target = int(input())
